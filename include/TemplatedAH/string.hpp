98cda53a4bb37acb0c10c00d3257d8fd66cc7abb
static <kmc7468@naver.com>
2018-08-06 20:49:46 +0900

    details::split_raw_string 템플릿 구조체 추가
    이 구조체는 tah::details::raw_string 구조체를 LF를 기준으로 tah::details::lines 구조체로 적절히 줄 단위로 나눠 변환합니다.

--- include/TemplatedAH/string.hpp
+++ include/TemplatedAH/string.hpp
@@ -23,12 +23,14 @@ namespace tah
 		struct line
 		{
 			static constexpr char32_t value[] = { String_... };
+			static constexpr std::size_t length = sizeof(value) / sizeof(char32_t);
 		};
 		template<typename... Lines_>
 		struct lines
 		{
 			template<int Index_>
 			using get = typename get_type_by_index<Index_, Lines_...>::type;
+			static constexpr std::size_t line = sizeof...(Lines_);
 		};
 
 		template<char32_t... String_>
@@ -49,6 +51,38 @@ namespace tah
 		struct make_raw_string
 		{
 			using type = typename make_raw_string_internal<String_, 0, sizeof(String_::str) / sizeof(char32_t) - 1>::type;
+		};
+
+		template<typename String_, bool NewLine_, std::size_t Index_, std::size_t Length_, char32_t Character_, char32_t... String2_>
+		struct split_raw_string_internal
+		{
+			using type = typename split_raw_string_internal<String_, NewLine_, Index_ + 1, Length_, String_::value[Index_], String2_..., Character_>::type;
+		};
+		template<typename String_, bool NewLine_, std::size_t Length_, char32_t Character_, char32_t... String2_>
+		struct split_raw_string_internal<String_, NewLine_, 0, Length_, Character_, String2_...>
+		{
+			using type = typename split_raw_string_internal<String_, NewLine_, 1, Length_, String_::value[0], String2_...>::type;
+		};
+		template<typename String_, bool NewLine_, std::size_t Index_, std::size_t Length_, char32_t... String2_>
+		struct split_raw_string_internal<String_, NewLine_, Index_, Length_, U'\n', String2_...>
+		{
+			using type = lines<line<String2_...>, typename split_raw_string_internal<String_, true, Index_ + 1, Length_, String_::value[Index_]>::type>;
+		};
+		template<typename String_, std::size_t Length_, char32_t Character_, char32_t... String2_>
+		struct split_raw_string_internal<String_, true, Length_, Length_, Character_, String2_...>
+		{
+			using type = line<String2_..., Character_>;
+		};
+		template<typename String_, std::size_t Length_, char32_t Character_, char32_t... String2_>
+		struct split_raw_string_internal<String_, false, Length_, Length_, Character_, String2_...>
+		{
+			using type = lines<line<String2_..., Character_>>;
+		};
+
+		template<typename String_>
+		struct split_raw_string
+		{
+			using type = typename split_raw_string_internal<String_, false, 0, String_::length, 0>::type;
 		};
 	}
 }
