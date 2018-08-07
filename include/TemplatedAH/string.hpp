d7aabde27c084ea361ea4451f37c055e1f9d0fe0
static <kmc7468@naver.com>
2018-08-07 18:41:06 +0900

    details::divide_hangul 템플릿 구조체 추가
    이 구조체는 유니코드 코드 포인트에서 0xAC00 ~ 0xD7A3 영역 사이에 있는 완성된 현대 한글 문자를 초성, 중성, 종성으로 나눕니다.

--- include/TemplatedAH/string.hpp
+++ include/TemplatedAH/string.hpp
@@ -84,6 +84,47 @@ namespace tah
 		{
 			using type = typename split_raw_string_internal<String_, false, 0, String_::length, 0>::type;
 		};
+
+		struct hangul
+		{
+			static constexpr char32_t chosungs[] =
+			{
+				U'ぁ', U'あ', U'い', U'ぇ', U'え', U'ぉ', U'け', U'げ', U'こ', U'さ', U'ざ', U'し', U'じ',
+				U'す', U'ず', U'せ', U'ぜ', U'そ', U'ぞ'
+			};
+			static constexpr char32_t jungsungs[] =
+			{
+				U'た', U'だ', U'ち', U'ぢ', U'っ', U'つ', U'づ', U'て', U'で', U'と', U'ど', U'な', U'に',
+				U'ぬ', U'ね', U'の', U'は', U'ば', U'ぱ', U'ひ', U'び'
+			};
+			static constexpr char32_t jongsungs[] =
+			{
+				0, U'ぁ', U'あ', U'ぃ', U'い', U'ぅ', U'う', U'ぇ', U'ぉ', U'お', U'か', U'が', U'き', U'ぎ',
+				U'く', U'ぐ', U'け', U'げ', U'ご', U'さ', U'ざ', U'し', U'じ', U'ず', U'せ', U'ぜ', U'そ', U'ぞ'
+			};
+			template<char32_t Character_>
+			static constexpr bool is_hangul = Character_ >= 0xAC00 && Character_ <= 0xD7A3;
+		};
+
+		template<char32_t Hangul_, typename = void>
+		struct divide_hangul;
+
+		template<char32_t Hangul_>
+		struct divide_hangul<Hangul_, typename std::enable_if<hangul::is_hangul<Hangul_>>::type>
+		{
+			static constexpr char32_t chosung =
+				hangul::chosungs[((((Hangul_ - 0xAC00) - ((Hangul_ - 0xAC00) % 28)) / 28) - ((((Hangul_ - 0xAC00) - ((Hangul_ - 0xAC00) % 28)) / 28) % 21)) / 21];
+			static constexpr char32_t jungsung =
+				hangul::jungsungs[(((Hangul_ - 0xAC00) - ((Hangul_ - 0xAC00) % 28)) / 28) % 21];
+			static constexpr char32_t jongsung =
+				hangul::jongsungs[(Hangul_ - 0xAC00) % 28];
+			static constexpr bool error = false;
+		};
+		template<char32_t Hangul_>
+		struct divide_hangul<Hangul_, typename std::enable_if<!hangul::is_hangul<Hangul_>>::type>
+		{
+			static constexpr bool error = true;
+		};
 	}
 }
 
