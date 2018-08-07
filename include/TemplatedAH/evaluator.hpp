3fa19f18f5feabf721dba683bf3ed1ea236c3545
static <kmc7468@naver.com>
2018-08-07 21:02:13 +0900

    details::move_to_cursor 템플릿 구조체 추가
    이 구조체는 커서가 아희 코드 영역 밖으로 벗어났을 때 아희 표준에서 명시한 예외 처리를 수행합니다.

--- include/TemplatedAH/evaluator.hpp
+++ include/TemplatedAH/evaluator.hpp
@@ -4,6 +4,7 @@
 #include "string.hpp"
 
 #include <cstddef>
+#include <type_traits>
 
 namespace tah
 {
@@ -13,10 +14,17 @@ namespace tah
 	{
 		enum class direction
 		{
-			left = 1,
+			left = 2,
 			right = ~left,
-			up = 2,
+			up = 4,
 			down = ~up,
+		};
+
+		template<direction Direction_>
+		struct reverse_direction
+		{
+			static constexpr direction value =
+				static_cast<direction>(~static_cast<int>(Direction_));
 		};
 
 		template<std::size_t X_, std::size_t Y_, std::size_t Speed_, tah::details::direction Direction_>
@@ -52,6 +60,53 @@ namespace tah
 			using storages = Storages_;
 			using cursor = Cursor_;
 			using storage = typename Storages_::template get<get_jongsung_index<SelectedStorage_>::value>;
+		};
+
+		template<typename Lines_, typename Cursor_, typename = void>
+		struct move_to_cursor;
+
+		template<typename Lines_, typename Cursor_>
+		struct move_to_cursor<Lines_, Cursor_,
+			typename std::enable_if<Cursor_::y < Lines_::line &&
+									Cursor_::x < Lines_::template get<Cursor_::y>::length>::type>
+		{
+			using type = Cursor_;
+		};
+		template<typename Lines_, typename Cursor_>
+		struct move_to_cursor<Lines_, Cursor_,
+			typename std::enable_if<Cursor_::y >= Lines_::line &&
+									Cursor_::x < Lines_::template get<Cursor_::y>::length &&
+									(Cursor_::y != static_cast<std::size_t>(-1) &&
+									 Cursor_::y != static_cast<std::size_t>(-2))>::type>
+		{
+			using type = cursor<Cursor_::x, 0, Cursor_::speed, Cursor_::direction>;
+		};
+		template<typename Lines_, typename Cursor_>
+		struct move_to_cursor<Lines_, Cursor_,
+			typename std::enable_if<Cursor_::y >= Lines_::line &&
+									Cursor_::x < Lines_::template get<Cursor_::y>::length &&
+									(Cursor_::y == static_cast<std::size_t>(-1) ||
+									 Cursor_::y == static_cast<std::size_t>(-2))>::type>
+		{
+			using type = cursor<Cursor_::x, Cursor_::line - 1, Cursor_::speed, Cursor_::direction>;
+		};
+		template<typename Lines_, typename Cursor_>
+		struct move_to_cursor<Lines_, Cursor_,
+			typename std::enable_if<Cursor_::y < Lines_::line &&
+									Cursor_::x >= Lines_::template get<Cursor_::y>::length &&
+									(Cursor_::x != static_cast<std::size_t>(-1) &&
+									 Cursor_::x != static_cast<std::size_t>(-2))>::type>
+		{
+			using type = cursor<0, Cursor_::y, Cursor_::speed, Cursor_::direction>;
+		};
+		template<typename Lines_, typename Cursor_>
+		struct move_to_cursor<Lines_, Cursor_,
+			typename std::enable_if<Cursor_::y < Lines_::line &&
+									Cursor_::x >= Lines_::template get<Cursor_::y>::length &&
+									(Cursor_::x == static_cast<std::size_t>(-1) ||
+									 Cursor_::x == static_cast<std::size_t>(-2))>::type>
+		{
+			using type = cursor<Cursor_::template get<Cursor_::y>::length - 1, Cursor_::y, Cursor_::speed, Cursor_::direction>;
 		};
 	}
 }
