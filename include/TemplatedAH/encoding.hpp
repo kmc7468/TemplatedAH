a43d195b722d9fdc591a6dcf7487b5577f33eb09
static <kmc7468@naver.com>
2018-08-10 20:47:11 +0900

    details::[utf16_to_utf32/utf32_to_utf16/utf8_to_utf16/utf16_to_utf8] 템플릿 구조체 추가
    이 템플릿 구조체들은 각각 UTF-16 to UTF-32, UTF-32 to UTF-16, UTF-8 to UTF-16, UTF-16 to UTF-8의 인코딩 변환을 수행합니다.

--- include/TemplatedAH/encoding.hpp
+++ include/TemplatedAH/encoding.hpp
@@ -153,6 +153,91 @@ namespace tah
 		{
 			using type = Output_;
 		};
+
+		template<typename String_, std::size_t Index_ = 0, typename Output_ = void, typename = void>
+		struct utf32_to_utf16;
+
+		template<typename String_, std::size_t Index_, typename Output_>
+		struct utf32_to_utf16<String_, Index_, Output_,
+			typename std::enable_if<String_::value[Index_] <= 0xFFFF &&
+									Index_ < String_::length>::type>
+		{
+			using type = typename add_raw_string<Output_,
+				typename add_raw_string<raw_string_utf16<static_cast<char16_t>(String_::value[Index_])>,
+				typename utf32_to_utf16<String_, Index_ + 1, void>::type
+				>::type
+			>::type;
+		};
+		template<typename String_, std::size_t Index_, typename Output_>
+		struct utf32_to_utf16<String_, Index_, Output_,
+			typename std::enable_if<(String_::value[Index_] > 0xFFFF) &&
+									Index_ < String_::length>::type>
+		{
+			using type = typename add_raw_string<Output_,
+				typename add_raw_string<
+					raw_string_utf16<
+						static_cast<char16_t>(static_cast<char16_t>(String_::value[Index_] - 0x10000) / 0x400 + 0xD800),
+						static_cast<char16_t>(static_cast<char16_t>(String_::value[Index_] - 0x10000) % 0x400 + 0xDC00)
+					>,
+					typename utf32_to_utf16<String_, Index_ + 1, void>::type
+				>::type
+			>::type;
+		};
+		template<typename String_, std::size_t Index_, typename Output_>
+		struct utf32_to_utf16<String_, Index_, Output_,
+			typename std::enable_if<Index_ >= String_::length>::type>
+		{
+			using type = Output_;
+		};
+
+		template<typename String_, std::size_t Index_ = 0, typename Output_ = void, typename = void>
+		struct utf16_to_utf32;
+
+		template<typename String_, std::size_t Index_, typename Output_>
+		struct utf16_to_utf32<String_, Index_, Output_,
+			typename std::enable_if<String_::value[Index_] >= 0xD800 && String_::value[Index_] <= 0xDBFF &&
+									Index_ < String_::length>::type>
+		{
+			using type = typename add_raw_string<Output_,
+				typename add_raw_string<
+					raw_string_utf32<
+						(String_::value[Index_] - 0xD800) * 0x400 +
+						(String_::value[Index_ + 1] - 0xDC00) + 0x10000
+					>,
+					typename utf16_to_utf32<String_, Index_ + 2, void>::type
+				>::type
+			>::type;
+		};
+		template<typename String_, std::size_t Index_, typename Output_>
+		struct utf16_to_utf32<String_, Index_, Output_,
+			typename std::enable_if<!(String_::value[Index_] >= 0xD800 && String_::value[Index_] <= 0xDBFF) &&
+									Index_ < String_::length>::type>
+		{
+			using type = typename add_raw_string<Output_,
+				typename add_raw_string<raw_string_utf32<String_::value[Index_]>,
+				typename utf16_to_utf32<String_, Index_ + 1, void>::type
+				>::type
+			>::type;
+		};
+		template<typename String_, std::size_t Index_, typename Output_>
+		struct utf16_to_utf32<String_, Index_, Output_,
+			typename std::enable_if<Index_ >= String_::length>::type>
+		{
+			using type = Output_;
+		};
+
+		template<typename String_>
+		struct utf8_to_utf16
+		{
+			using type = typename utf32_to_utf16<
+				typename utf8_to_utf32<String_>::type>::type;
+		};
+		template<typename String_>
+		struct utf16_to_utf8
+		{
+			using type = typename utf32_to_utf8<
+				typename utf16_to_utf32<String_>::type>::type;
+		};
 	}
 }
 
